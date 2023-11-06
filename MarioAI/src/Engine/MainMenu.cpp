7baee60d05b39a46f637f637de1010a942b8b283
BambiTheLama <szymek22466@gmail.com>
2023-11-06 18:24:05 +0100

    Zmiany estetyczne w kodzie

--- MarioAI/src/Engine/MainMenu.cpp
+++ MarioAI/src/Engine/MainMenu.cpp
@@ -5,67 +5,69 @@
 #include "MapEditor.h"
 #include "GameLearnScene.h"
 
-Rectangle AIGame;
-Rectangle startGame;
-Rectangle mapEditor;
-Texture2D background;
-Color buttonColor;
+Button::Button(Rectangle pos, std::string text)
+{
+    this->pos = pos;
+    this->text = text;
+}
+
+void Button::draw()
+{
+    DrawRectangleRounded(pos, 1.f, 0, GetColor(0x2850ffff));
+    const char* t = text.c_str();
+    DrawTextEx(CustomFont::customFont, t, Vector2{ pos.x + pos.width / 2 - MeasureTextEx(CustomFont::customFont, t, 64, 2).x / 2, pos.y + pos.height / 2 - 32 }, 64, 2, BLACK);
+}
+
+bool Button::press()
+{
+    return IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), pos);
+}
 
 MainMenu::MainMenu() {
-  SetExitKey(KEY_ESCAPE);
-  background = LoadTexture("res/background.png");
-  buttonColor = GetColor(0x2850ffff);
-
-  // Wyliczenie œrodkowych pozycji przycisków
-  const int screenWidth = GetScreenWidth();
-  const int screenHeight = GetScreenHeight();
-  const int buttonWidth = 350;
-  const int buttonHeight = 100;
-  const int buttonSpacing = 20;
-
-  const int buttonX = (screenWidth - buttonWidth) / 2;
-  const int aiGameButtonY = screenHeight / 3 - (buttonHeight + buttonSpacing);
-  const int startGameButtonY = screenHeight / 3;
-  const int mapEditorButtonY = screenHeight / 3 + (buttonHeight + buttonSpacing);
-
-  AIGame = { static_cast<float>(buttonX), static_cast<float>(aiGameButtonY), static_cast<float>(buttonWidth), static_cast<float>(buttonHeight) };
-  startGame = { static_cast<float>(buttonX), static_cast<float>(startGameButtonY), static_cast<float>(buttonWidth), static_cast<float>(buttonHeight) };
-  mapEditor = { static_cast<float>(buttonX), static_cast<float>(mapEditorButtonY), static_cast<float>(buttonWidth), static_cast<float>(buttonHeight) };
+    SetExitKey(KEY_ESCAPE);
+    background = LoadTexture("res/background.png");
+    
+    // Wyliczenie œrodkowych pozycji przycisków
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
+    const int buttonWidth = 350;
+    const int buttonHeight = 100;
+    const int buttonSpacing = 20;
+    
+    const int buttonX = (screenWidth - buttonWidth) / 2;
+    const int aiGameButtonY = screenHeight / 3 - (buttonHeight + buttonSpacing);
+    const int startGameButtonY = screenHeight / 3;
+    const int mapEditorButtonY = screenHeight / 3 + (buttonHeight + buttonSpacing);
+    
+    buttons[0] = new Button({ (float)buttonX, (float)aiGameButtonY, (float)buttonWidth, (float)buttonHeight }, "AI GAME");
+    buttons[1] = new Button({ (float)buttonX, (float)startGameButtonY, (float)buttonWidth, (float)buttonHeight }, "START GAME");
+    buttons[2] = new Button({ (float)buttonX, (float)mapEditorButtonY, (float)buttonWidth, (float)buttonHeight }, "MAP EDITOR");
 }
 MainMenu::~MainMenu()
 {
     UnloadTexture(background);
+    for (int i = 0; i < 3; i++)
+        delete buttons[i];
 }
 
 void MainMenu::update(float deltaTime) 
 {
-  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-    if (CheckCollisionPointRec(GetMousePosition(), AIGame))
-      Engine::getEngine()->setScene(new GameLearnScene());
-    if (CheckCollisionPointRec(GetMousePosition(), startGame)) {
-      SetExitKey(0);
-      Engine::getEngine()->setScene(new Game(false));
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        if (buttons[0]->press())
+            Engine::getEngine()->setScene(new GameLearnScene());
+        else if (buttons[1]->press()) {
+            SetExitKey(0);
+            Engine::getEngine()->setScene(new Game(false));
+        }
+        else if (buttons[2]->press())
+            Engine::getEngine()->setScene(new MapEditor());
     }
-    if (CheckCollisionPointRec(GetMousePosition(), mapEditor))
-      Engine::getEngine()->setScene(new MapEditor());
-  }
 }
 
 void MainMenu::draw() 
 {
-
-  DrawTexture(background, 0, 0, WHITE);
-
-  // Rysowanie zaokr¹glonych przycisków
-  DrawRectangleRounded(AIGame, 1.f, 0, buttonColor);
-  DrawRectangleRounded(startGame, 1.f, 0, buttonColor);
-  DrawRectangleRounded(mapEditor, 1.f, 0, buttonColor);
-
-  // Tekst na przyciskach
-  DrawTextEx(CustomFont::customFont, "AI GAME", Vector2{ AIGame.x + AIGame.width / 2 - MeasureTextEx(CustomFont::customFont, "AI GAME", 64, 2).x / 2, AIGame.y + AIGame.height / 2 - 32 }, 64, 2, BLACK);
-  DrawTextEx(CustomFont::customFont, "START GAME", Vector2{ startGame.x + startGame.width / 2 - MeasureTextEx(CustomFont::customFont, "START GAME", 64, 2).x / 2, startGame.y + startGame.height / 2 - 32 }, 64, 2, BLACK);
-  DrawTextEx(CustomFont::customFont, "MAP EDITOR", Vector2{ mapEditor.x + mapEditor.width / 2 - MeasureTextEx(CustomFont::customFont, "MAP EDITOR", 64, 2).x / 2, mapEditor.y + mapEditor.height / 2 - 32 }, 64, 2, BLACK);
-
-
-
+    DrawTexturePro(background, { 0,0,(float)background.width,(float)background.height }, { 0,0,(float)GetScreenWidth(),(float)GetScreenHeight() }, { 0,0 }, 0, WHITE);
+    //DrawTexture(background, 0, 0, WHITE);
+    for (int i = 0; i < 3; i++)
+        buttons[i]->draw();
 }
--- MarioAI/src/Engine/MainMenu.h
+++ MarioAI/src/Engine/MainMenu.h
@@ -1,8 +1,23 @@
 #pragma once
 #include "Scene.h"
+#include "raylib.h"
+#include <string>
+class Button {
+    Rectangle pos;
+    std::string text;
+public:
+    Button(Rectangle pos, std::string text);
+
+    void draw();
+
+    virtual bool press();
+};
+
 class MainMenu :
     public Scene
 {
+    Button *buttons[3];
+    Texture2D background;
 public:
 
     MainMenu();
